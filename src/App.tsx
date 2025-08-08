4a9c824768f4fcf62b3c024be9df04f0b88f5a5a
mrTebrom <mr.tebron@gmail.com>
2025-08-08 16:14:35 +0600

    feat(router): add basic routes with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import { Layout } from './layout/layout';
+
 export const App = () => {
-  return <></>
+  return <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 }
