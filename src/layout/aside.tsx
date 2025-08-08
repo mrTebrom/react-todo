bac20b164df9580f690489f1c71145f75769832b
mrTebrom <mr.tebron@gmail.com>
2025-08-08 17:57:33 +0600

    feat(layout): add basic aside navigation with task sections

---
+++ src/layout/aside.tsx
@@ -0,0 +1,68 @@
+import { Divider, Flex, Layout, Menu, Button } from "antd";
+import {
+    InboxOutlined,
+    CalendarOutlined,
+    CheckCircleOutlined,
+    DeleteOutlined,
+    ClockCircleOutlined,
+    PlusOutlined,
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+const { Sider } = Layout;
+export const AsideApp = () => {
+    const basicLink: MenuProps["items"] = [
+        {
+            key: "inbox",
+            label: "Входящие",
+            icon: <InboxOutlined />,
+        },
+        {
+            key: "today",
+            label: "Сегодня",
+            icon: <CalendarOutlined />,
+        },
+        {
+            key: "upcoming",
+            label: "Предстоящие",
+            icon: <ClockCircleOutlined />,
+        },
+        {
+            key: "completed",
+            label: "Завершённые",
+            icon: <CheckCircleOutlined />,
+        },
+        {
+            key: "trash",
+            label: "Корзина",
+            icon: <DeleteOutlined />,
+        },
+    ];
+    return (
+        <>
+            <Sider width={200} style={{ backgroundColor: "#ffffff" }}>
+                <Menu
+                    mode="inline"
+                    defaultSelectedKeys={["1"]}
+                    defaultOpenKeys={["sub1"]}
+                    items={basicLink}
+                />
+
+                <Flex
+                    vertical={true}
+                    style={{
+                        paddingLeft: 24,
+                        paddingRight: 12,
+                    }}
+                >
+                    <Divider />
+                    <Flex justify="space-between" align="center">
+                        <span>Проекты</span>
+                        <Button size="small" shape="circle" type="text">
+                            <PlusOutlined />
+                        </Button>
+                    </Flex>
+                </Flex>
+            </Sider>
+        </>
+    );
+};
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,36 +1,11 @@
 import React from "react";
-import {
-    LaptopOutlined,
-    NotificationOutlined,
-    UserOutlined,
-} from "@ant-design/icons";
-import type { MenuProps } from "antd";
+
 import { Breadcrumb, Layout, Menu, theme } from "antd";
 import { Outlet } from "react-router-dom";
 import { HeaderApp } from "./header";
+import { AsideApp } from "./aside";
 
-const { Content, Sider } = Layout;
-
-const items2: MenuProps["items"] = [
-    UserOutlined,
-    LaptopOutlined,
-    NotificationOutlined,
-].map((icon, index) => {
-    const key = String(index + 1);
-
-    return {
-        key: `sub${key}`,
-        icon: React.createElement(icon),
-        label: `subnav ${key}`,
-        children: Array.from({ length: 4 }).map((_, j) => {
-            const subKey = index * 4 + j + 1;
-            return {
-                key: subKey,
-                label: `option${subKey}`,
-            };
-        }),
-    };
-});
+const { Content } = Layout;
 
 export const LayoutApp: React.FC = () => {
     const {
@@ -42,15 +17,7 @@ export const LayoutApp: React.FC = () => {
         <Layout style={{ height: "100vh" }}>
             <HeaderApp />
             <Layout>
-                <Sider width={width} style={{ background: colorBgContainer }}>
-                    <Menu
-                        mode="inline"
-                        defaultSelectedKeys={["1"]}
-                        defaultOpenKeys={["sub1"]}
-                        style={{ height: "100%", borderInlineEnd: 0 }}
-                        items={items2}
-                    />
-                </Sider>
+                <AsideApp />
                 <Layout style={{ padding: "0 24px 24px" }}>
                     <Breadcrumb
                         items={[
